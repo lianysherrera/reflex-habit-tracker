de6cfbcd4342874798c81da5e081848b79aa7f67
Liany <lianys.herrera@hotmail.com>
2026-05-21 17:06:46 +0000

    feat: modelo HabitLog en base de datos

--- reflex_habit_tracker/models.py
+++ reflex_habit_tracker/models.py
@@ -1,7 +1,14 @@
 from typing import Optional
+from datetime import date
 from sqlmodel import Field, sqlModel
 
 class Habit(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     emoji: str = "✅"
+
+#Modelo de registro diario
+class HabitLog(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    habit_id: int Field(foreign_key="habit.id")
+    date: date = Field(default_factory=date.today)
